2e20e7453cf7932f84d23254cd2b53196f886965
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-01-06 02:41:10 -0300

    adicionando o projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Button from './components/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Olá React</h1>
+      <Button title= "Entrar"/>
+      <Button title= "Fechar"/>
+      <Button title= "Abrir"/>
+      <Button title= "Salvar"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppClass.js
@@ -0,0 +1,18 @@
+//Exemplo de componente de Classe
+
+import React, { Component } from 'react';
+
+export default class AppClass extends Component {
+    constructor(props){
+        super(props);
+        this.state = { counter: 0 };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+
+    }
+    render(){
+        return <div>AppClass</div>
+    }
+}
